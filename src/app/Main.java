527a96f7840a0ef543d78868d8cb3d1eaccd6fdb
Vlarik <i>
2024-11-24 18:24:32 +0300

    save

---
+++ src/app/Main.java
@@ -0,0 +1,10 @@
+package app;
+
+
+public class Main {
+    public static void main(String[] args) {
+        Database database = new Database();
+        Menu menu = new Menu(database);
+        menu.displayMenu();
+    }
+}
